7a8af46c94021c71b413daf4a13ed786dfed5b86
Rushikesh Gaikwad <grushikesh907@gmail.com>
2023-01-12 14:17:14 +0530

    React app created

---
+++ client/src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <p className="text-slate-500 font-extrabold">Hello</p>
+    </div>
+  );
+}
+
+export default App;
